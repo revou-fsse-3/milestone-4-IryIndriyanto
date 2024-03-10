344e8fa09de17a9a2c09c04476ef6e0cf3d7c551
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-10 06:41:38 -0700

    feat : create account table model

---
+++ models/account.py
@@ -0,0 +1,13 @@
+from db import db
+
+
+class AccountModel(db.Model):
+    __tablename__ = "account"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    account_type = db.Column(db.String(255))
+    account_number = db.Column(db.String(255), unique=True, nullable=False)
+    balance = db.Column(db.Float(10, 2), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
