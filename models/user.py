b69645fb694c743169ede61dafe587c12146a7b4
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-10 06:42:42 -0700

    refactor : add more configuration

--- models/user.py
+++ models/user.py
@@ -4,9 +4,9 @@ from db import db
 class UserModel(db.Model):
     __tablename__ = "user"
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True)
-    email = db.Column(db.String(255), unique=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
