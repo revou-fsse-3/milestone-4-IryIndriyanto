be8b10cd250ef989b8e15e87f6f40349f5eea741
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-11 05:35:36 -0700

    feat : create update user info route

--- controllers/user.py
+++ controllers/user.py
@@ -19,12 +19,12 @@ blp = Blueprint("users", "users", description="data of users", url_prefix="/user
 @blp.arguments(UserSchema)
 @blp.response(200)
 def user_register(user_data):
-    new_user = UserModel(
-        username=user_data["username"],
-        email=user_data["email"],
-        password_hash=pbkdf2_sha256.hash(user_data["password"]),
-    )
     try:
+        new_user = UserModel(
+            username=user_data["username"],
+            email=user_data["email"],
+            password_hash=pbkdf2_sha256.hash(user_data["password"]),
+        )
         db.session.add(new_user)
         db.session.commit()
     except IntegrityError:
@@ -51,7 +51,34 @@ def user_login(user_data):
 @blp.response(200)
 def get_user_profile():
     user_id = get_jwt_identity()
-    user = UserModel.query.get(user_id)
+    user = db.session.get(UserModel, user_id)
     if user:
         return jsonify({"username": user.username, "email": user.email}), 200
+    return jsonify({"message": "User not found"}), 404
+
+
+@blp.route("/me", methods=["PUT"])
+@blp.arguments(UserSchema)
+@jwt_required()
+@blp.response(200)
+def edit_user_profile(user_data):
+    user_id = get_jwt_identity()
+    user = db.session.get(UserModel, user_id)
+    if user:
+        try:
+            updated_data = {
+                "username": user_data.get("username", user.username),
+                "email": user_data.get("email", user.email),
+            }
+
+            if "password" in user_data:
+                updated_data["password_hash"] = pbkdf2_sha256.hash(
+                    user_data["password"]
+                )
+
+            UserModel.query.filter_by(id=user_id).update(updated_data)
+            db.session.commit()
+            return jsonify({"message": "User updated"}), 200
+        except IntegrityError:
+            return jsonify({"Error": "Username or email already exists."}), 400
     return jsonify({"message": "User not found"}), 404
