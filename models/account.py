0934fdbd2d81566e0d8249ded676625b9d0e9b70
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-12 06:53:27 -0700

    fix : minor fix

--- models/account.py
+++ models/account.py
@@ -6,8 +6,10 @@ class AccountModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    account_type = db.Column(db.String(255))
+    account_type = db.Column(db.String(255), nullable=False)
     account_number = db.Column(db.String(255), unique=True, nullable=False)
     balance = db.Column(db.Float(10, 2), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
+    created_at = db.Column(
+        db.DateTime(timezone=True), default=db.func.now(), nullable=False
+    )
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
--- models/transaction.py
+++ models/transaction.py
@@ -7,7 +7,7 @@ class TransactionModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     from_account_id = db.Column(db.Integer, db.ForeignKey("account.id"), nullable=False)
     to_account_id = db.Column(db.Integer, db.ForeignKey("account.id"), nullable=False)
-    balance = db.Column(db.Float(10, 2), nullable=False)
+    amount = db.Column(db.Float(10, 2), nullable=False)
     type = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255))
     created_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
