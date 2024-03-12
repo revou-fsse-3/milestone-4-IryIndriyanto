015bb2b2fc550ae8644c93927b1003c5957d1317
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-12 06:55:45 -0700

    refac : remove unnecessary schema

--- schemas/account.py
+++ schemas/account.py
@@ -1,10 +1,4 @@
 from marshmallow import Schema, fields
 
 class AccountSchema(Schema):
-    id = fields.Int()
-    user_id = fields.Int()
     account_type = fields.Str()
-    account_number = fields.Str()
-    balance = fields.Float()
-    created_at = fields.DateTime()
-    updated_at = fields.DateTime()
