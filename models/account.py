f258a917a3d882371ba337bc416c2705a6354b30
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-13 05:50:44 -0700

    feat : adding more configuration

--- models/account.py
+++ models/account.py
@@ -4,12 +4,12 @@ from db import db
 class AccountModel(db.Model):
     __tablename__ = "account"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, dumb_only=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, dumb_only=True)
     account_type = db.Column(db.String(255), nullable=False)
-    account_number = db.Column(db.String(255), unique=True, nullable=False)
+    account_number = db.Column(db.String(255), unique=True, nullable=False, dumb_only=True)
     balance = db.Column(db.Float(10, 2), nullable=False)
     created_at = db.Column(
-        db.DateTime(timezone=True), default=db.func.now(), nullable=False
+        db.DateTime(timezone=True), default=db.func.now(), nullable=False, dumb_only=True
     )
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now(), dumb_only=True)
