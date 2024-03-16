19d7226dd2d0dc132f8f1157262f8088a9e44428
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-03-16 01:13:09 -0700

    refac : formatting

--- models/user.py
+++ models/user.py
@@ -8,5 +8,9 @@ class UserModel(db.Model):
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
-    updated_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), onupdate=db.func.now())
+    created_at = db.Column(
+        db.DateTime(timezone=True), default=db.func.now(), nullable=False
+    )
+    updated_at = db.Column(
+        db.DateTime(timezone=True), default=db.func.now(), onupdate=db.func.now()
+    )
